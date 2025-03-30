6d3b5feef97abdbf52a312981c3136b94bd6c87e
Ryan Ewing <ewingryan98@gmail.com>
2025-03-29 18:26:21 -0700

    API scaffolding

--- backend/controlplane-api/src/main.rs
+++ backend/controlplane-api/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use controlplane_api::routes;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(routes::root))
+        // `POST /users` goes to `create_user`
+        .route("/users", post(routes::create_user));
+
+    // run our app with hyper
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
