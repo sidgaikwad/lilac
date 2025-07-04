79ae5742a976c643bc56c120163f474b7a95b366
Lucas Ewing <Lucasbewing@gmail.com>
2025-07-03 21:07:53 -0700

    stopped rendering providers "or" if none exist

--- frontend/ml-pipeline-frontend/src/features/account/pages/login-page.tsx
+++ frontend/ml-pipeline-frontend/src/features/account/pages/login-page.tsx
@@ -124,27 +124,31 @@ function LoginPage() {
             </span>
           </div>
         </form>
-        <div className="relative my-4">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
-          </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-card px-2 text-muted-foreground">
-              or
-            </span>
-          </div>
-        </div>
-        <div className="space-y-2">
-          {providersLoading ? (
-            <div className="flex justify-center">
-              <Spinner />
+        {providers && providers.length > 0 && (
+          <>
+            <div className="relative my-4">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-card px-2 text-muted-foreground">
+                  or
+                </span>
+              </div>
             </div>
-          ) : (
-            providers?.map((provider) => (
-              <ProviderLoginButton key={provider.name} provider={provider} />
-            ))
-          )}
-        </div>
+            <div className="space-y-2">
+              {providersLoading ? (
+                <div className="flex justify-center">
+                  <Spinner />
+                </div>
+              ) : (
+                providers?.map((provider) => (
+                  <ProviderLoginButton key={provider.name} provider={provider} />
+                ))
+              )}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
